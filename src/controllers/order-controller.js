353c3dd8adc736c0f4158faaba72f89104d6a0d9
ThanyawitNiti <thanyawit.nitipornwattana@gmail.com>
2023-10-23 14:32:33 +0700

    Create OrderProduct Table

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -1,34 +1,58 @@
-const { defaults } = require('joi')
-const prisma = require('../models/prisma')
+const { defaults } = require("joi");
+const prisma = require("../models/prisma");
 
-exports.createOrder = async (req,res,next) =>{
-    try{
-       console.log('####',req.body)
-       const  order = req.body.map((el) =>{
-        return el.user_id
-       })
-       const indexOrder = 1
-       console.log(order)
-       const orderTotal =req.body.reduce((acc,item)=>{
-        let total = item.amount * item.product.price
-        acc+= total
-        return acc
-    },0)
-    console.log(orderTotal)
+exports.createOrder = async (req, res, next) => {
+  try {
+    console.log("####", req.body);
+    const user_id = req.body.map((el) => {
+      return el.user_id;
+    });
+    const indexUserId = 1;
+    console.log(user_id);
+    const orderTotal = req.body.reduce((acc, item) => {
+      let total = item.amount * item.product.price;
+      acc += total;
+      return acc;
+    }, 0);
+    console.log(orderTotal);
     const orderFromUser = await prisma.order.create({
-        data:{
-            user_id: order[indexOrder],
-            total_price:orderTotal.toString(),
-            payment_status:false
-            }
-        }
-    )
-    console.log(orderFromUser)
-    console.log('2222222222',orderFromUser.id)
-    
+      data: {
+        user_id: user_id[indexUserId],
+        total_price: orderTotal.toString(),
+        payment_status: false,
+      },
+    });
+    console.log(orderFromUser);
+    console.log("###ID###", orderFromUser.id);
+    // const quantity = req.body.map((el)=>{
+    //     return el.amount
+    // })
+    // console.log(quantity)
+const orderProductTable = []
 
-    res.status(200).json({ message: "Order table created" })
-    }catch(err){
-        console.log(err)
+    for (const reqBody of req.body) {
+      const { id,user_id, amount, product_id } = reqBody;
+      const orderProduct = await prisma.orderProduct.create({
+        data: {
+          quantity: amount.toString(),
+          product:{
+            connect:{
+                id: product_id,
+            }
+          },
+          order:{
+            connect:{
+                id:orderFromUser.id
+            }
+          },
+        },
+      });
+      orderProductTable.push(orderProduct)
     }
-}
+  console.log(orderProductTable)
+
+    res.status(200).json({ message: "Order table created" });
+  } catch (err) {
+    console.log(err);
+  }
+};
